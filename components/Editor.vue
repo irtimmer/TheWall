6071cb33c28128b24e89d92a1df4bcc58d1526e1
Iwan Timmer <irtimmer@gmail.com>
2024-06-29 18:16:36 +0200

    controller: add save button to commit changes to renderer

--- components/Editor.vue
+++ components/Editor.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="relative mt-6 flex-1 px-4 sm:px-6">
-    <SelectButton v-model="view.type" :options="viewTypes" optionLabel="label" optionValue="value" class="mb-2" />
-    <div v-if="view.type === 'layout'" class="flex">
-      <InputText type="text" v-model="view.constraints.width" class="my-2 flex-1 w-0" />
-      <InputText type="text" v-model="view.constraints.height" class="my-2 flex-1 w-0" />
+    <SelectButton v-model="internalView.type" :options="viewTypes" optionLabel="label" optionValue="value" class="mb-2" />
+    <div v-if="internalView.type === 'layout'" class="flex">
+      <InputText type="text" v-model="internalView.constraints.width" class="my-2 flex-1 w-0" />
+      <InputText type="text" v-model="internalView.constraints.height" class="my-2 flex-1 w-0" />
     </div>
-    <InputText v-else type="text" v-model="view.url" class="my-2" />
+    <InputText v-else type="text" v-model="internalView.url" class="my-2" />
+    <Button @click="save">Save</Button>
   </div>
 </template>
 
@@ -13,6 +14,8 @@
 const props = defineProps<{
   view: View
 }>()
+
+const internalView = ref(structuredClone(toRaw(props.view)))
 
 const viewTypes = [
   { label: 'Layout', value: 'layout'},
@@ -31,4 +34,12 @@ watch(() => props.view.type, async (type, oldType) => {
     delete props.view.views
   }
 })
+
+function save() {
+  for (let key in props.view)
+    if (props.view.hasOwnProperty(key))
+      delete props.view[key]
+
+  Object.assign(props.view, structuredClone(toRaw(internalView.value)))
+}
 </script>
