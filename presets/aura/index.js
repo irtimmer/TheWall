9872119953489969949f3e59f669bdf7e4bac95c
Iwan Timmer <irtimmer@gmail.com>
2024-06-30 22:46:19 +0200

    ui: add missing Auro preset files

---
+++ presets/aura/index.js
@@ -0,0 +1,18 @@
+import button from './button';
+import inputtext from './inputtext';
+import selectbutton from './selectbutton';
+import sidebar from './sidebar';
+
+export default {
+    global,
+
+    //forms
+    inputtext,
+    selectbutton,
+
+    //buttons
+    button,
+
+    //overlays
+    sidebar,
+};
