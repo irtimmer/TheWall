4d1e427cdc03c2315d78275e719330332f525844
Iwan Timmer <irtimmer@gmail.com>
2024-07-21 13:06:53 +0200

    server: send current layout when renderer connects

--- server/api/events.ts
+++ server/api/events.ts
@@ -6,6 +6,14 @@ export default defineEventHandler(async (event) => {
   if (!id)
     throw new Error('id is required')
 
+  // Notify the client of the current state
+  const currentState = getState(id.toString())
+  if (currentState)
+    await eventStream.push(JSON.stringify({
+      action: 'setup',
+      data: currentState
+    }))
+
   registerReceiver(id.toString(), async (data) => {
     await eventStream.push(JSON.stringify(data))
   })
