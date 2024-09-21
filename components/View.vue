2b2fba79e7ec4d1f39d2c3a19d487378cb021f7a
Iwan Timmer <irtimmer@gmail.com>
2024-09-21 21:29:46 +0200

    view: add transitions to move and resize actions on view

--- components/View.vue
+++ components/View.vue
@@ -2,7 +2,7 @@
 <!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
 
 <template>
-  <div :style="style">
+  <div @transitionend="endTransition" :style="style">
     <iframe v-if="view.type == 'iframe'" @load="frameLoaded" :src="view.url" ref="iframeView" />
     <img v-else-if="view.type == 'img'" :src="view.url" />
     <video v-else-if="view.type == 'video'" :src="view.url" autoplay />
@@ -27,7 +27,41 @@ const props = defineProps<{
   view: View
 }>()
 
+const scaleModel = ref({
+  transition: true,
+  width: props.view.width,
+  height: props.view.height,
+  scaleX: 1,
+  scaleY: 1
+})
+
+watch(() => props.view, view => {
+  scaleModel.value.transition = true
+  scaleModel.value.scaleX = view.width / scaleModel.value.width
+  scaleModel.value.scaleY = view.height / scaleModel.value.height
+})
+
 const iframeView = ref<HTMLIFrameElement | null>(null)
+const style = computed(() => {
+  return {
+    position: 'absolute',
+    height: scaleModel.value.height + 'vh',
+    width: scaleModel.value.width + 'vw',
+    'transform-origin': 'top left',
+    transform: `translate(${props.view.left}vw, ${props.view.top}vh) scale(${scaleModel.value.scaleX}, ${scaleModel.value.scaleY})`,
+    transition: scaleModel.value.transition ? `transform 0.5s ease-in-out` : null
+  }
+})
+
+const endTransition = () => {
+  scaleModel.value = {
+    transition: false,
+    width: props.view.width,
+    height: props.view.height,
+    scaleX: 1,
+    scaleY: 1
+  }
+}
 
 watch(() => props.view.css, (css, oldCss) => {
   if (window.wallInjectCss && iframeView.value && css)
