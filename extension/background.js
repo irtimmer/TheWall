85f20669d9c2f709687e220307dc678388d7583b
Iwan Timmer <irtimmer@gmail.com>
2024-09-24 21:14:50 +0200

    extension: extend header modification to all requests while change CSP headers more precise

--- extension/background.js
+++ extension/background.js
@@ -1,16 +1,34 @@
 // Copyright (C) 2024 Iwan Timmer
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-const REMOVE_HEADERS = ['x-frame-options', 'frame-options', 'content-security-policy']
+const REMOVE_HEADERS = ['x-frame-options', 'frame-options']
 
 let trustedOrigin = null
 
 const onHeadersReceived = req => {
-  let responseHeaders = req.responseHeaders.filter(header => !REMOVE_HEADERS.includes(header.name.toLowerCase()))
+  // Only modify headers for iframes on the top level
+  if (req.parentFrameId != 0)
+    return {}
+
+  let responseHeaders = req.responseHeaders
+  if (req.type == 'sub_frame')
+    responseHeaders = req.responseHeaders.filter(header => !REMOVE_HEADERS.includes(header.name.toLowerCase()))
+
   for (let header of responseHeaders) {
-    // Replace SameSite=Lax and SameSite=Strict with SameSite=None to allow cross-origin cookies
-    if (header.name.toLowerCase() === 'set-cookie') {
-      header.value = header.value.replace(/SameSite=Lax|SameSite=Strict/i, 'SameSite=None')
+    const headerName = header.name.toLowerCase()
+    switch (headerName) {
+      case 'set-cookie':
+        // Replace SameSite=Lax and SameSite=Strict with SameSite=None to allow cookies in iframes
+        header.value = header.value.replace(/SameSite=Lax|SameSite=Strict/i, 'SameSite=None')
+        break
+      case 'content-security-policy':
+      case 'content-security-policy-report-only':
+        if (req.type != 'sub_frame')
+          break
+
+        // Allow all content to be loaded in iframes
+        header.value = header.value.replace(/frame-ancestors [^;]+;?/i, '')
+        break
     }
   }
   return { responseHeaders }
@@ -28,7 +46,6 @@ browser.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
     browser.webRequest.onHeadersReceived.addListener(
       onHeadersReceived, {
         urls: [ '<all_urls>' ],
-        types: [ 'sub_frame' ],
         tabId: message.tabId
       },
       ['blocking', 'responseHeaders']
