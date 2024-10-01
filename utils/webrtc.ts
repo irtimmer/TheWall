72595f8ca83e5f6188f241acd826ae453b48bba0
Iwan Timmer <irtimmer@gmail.com>
2024-10-01 23:59:41 +0200

    webrtc: correct useRuntimeConfig

--- utils/webrtc.ts
+++ utils/webrtc.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 export function useWebRTC(polite: boolean, signal: (signal: {description?: RTCSessionDescription, candidate?: RTCIceCandidate}) => Promise<void>, remoteView: HTMLVideoElement | undefined) {
-  const config = useAppConfig()
+  const config = useRuntimeConfig()
 
   let makingOffer = false;
   let ignoreOffer = false;
